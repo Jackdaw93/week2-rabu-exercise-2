df4d255d8dfc7ce814fa9551dfd777d3f7d2b96a
Rohmad <rohmad37@ymail.com>
2020-06-18 06:55:15 +0700

    'Revisi dari Mentor'

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
-let data = [];
+let divList = document.querySelector(".display-todo");
+let listTodo = [];
+let addButton = document.getElementById("add-button");
 
-function newElement() {
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("myInput").value;
-  let t = document.createTextNode(inputValue);
-  li.appendChild(t);
+displayTodos = () => {
+  divList.innerHTML = null;
 
-  document.getElementById("myUL").appendChild(li);
+  listTodo.forEach((item, index) => {
+    let itemTodo = document.createTextNode(item);
+    let textTodo = document.createElement("p");
+    textTodo.appendChild(itemTodo);
+    let divTodo = createDiv("class", "todo").appendChild(textTodo);
 
-  let edit = document.createElement("BUTTON");
-  let editText = document.createTextNode("Edit");
+    let divButton = createDiv("class", "button");
+    let editbutton = createButton("edit");
+    let deleteButton = createButton("delete");
+    divButton.appendChild(editbutton);
+    divButton.appendChild(deleteButton);
 
-  edit.appendChild(editText);
-  li.appendChild(edit);
+    let list = createDiv("class", "list");
+    list.appendChild(divTodo);
+    list.appendChild(divButton);
+    divList.appendChild(list);
 
-  let hapus = document.createElement("BUTTON");
-  let hapusText = document.createTextNode("Hapus");
+    editbutton.addEventListener("click", () => {
+      editItem(index);
+    });
+    deleteButton.addEventListener("click", () => {
+      deleteItem(index);
+    });
+  });
+};
 
-  hapus.appendChild(hapusText);
-  li.appendChild(hapus);
-}
+createButton = (element) => {
+  let button = document.createElement("button");
+  button.setAttribute("type", "button");
+  button.setAttribute("class", `btn btn-primary btn-sm ${element}`);
+  button.setAttribute("id", `${element}`);
+  let buttonText = document.createTextNode(`${element}`);
+  button.appendChild(buttonText);
+  // console.log(button);
+  return button;
+};
 
-const button = document.getElementById("add");
-button.addEventListener("click", newElement);
+createDiv = (attName, valueAtt) => {
+  let div = document.createElement("div");
+  div.setAttribute(attName, valueAtt);
+  return div;
+};
+
+editItem = (index) => {
+  newItem = prompt("Mau edit jadi apa?");
+  listTodo.splice(index, 1, newItem);
+
+  displayTodos();
+};
+
+deleteItem = (index) => {
+  listTodo.splice(index, 1);
+
+  displayTodos();
+};
+
+addButton.addEventListener("click", () => {
+  const inputTodo = document.getElementById("inputTodo").value;
+  listTodo.push(inputTodo);
+  displayTodos();
+});
